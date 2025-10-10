9d136b69a2a19fafe3702acea2bf4db6b846727c
ibrahim9989 <ibrahimshaheer92@gmail.com>
2025-10-11 02:26:27 +0530

    Transform navbar to Apple-style design with search icon and centered navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -31,8 +31,18 @@ export default function Navigation() {
           <li><Link href="#about" onClick={closeMenu} data-cursor-hover>About</Link></li>
           <li><Link href="#expertise" onClick={closeMenu} data-cursor-hover>Services</Link></li>
           <li><Link href="#work" onClick={closeMenu} data-cursor-hover>Work</Link></li>
-          <li><Link href="#contact" className={styles.navCta} onClick={closeMenu} data-cursor-hover>Start Project</Link></li>
+          <li><Link href="#contact" onClick={closeMenu} data-cursor-hover>Contact</Link></li>
         </ul>
+        <div className={styles.navActions}>
+          <button className={styles.searchButton} aria-label="Search">
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M21 21L16.514 16.506L21 21ZM19 10.5C19 15.194 15.194 19 10.5 19C5.806 19 2 15.194 2 10.5C2 5.806 5.806 2 10.5 2C15.194 2 19 5.806 19 10.5Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </button>
+          <Link href="#contact" className={styles.ctaButton} data-cursor-hover>
+            Start Project
+          </Link>
+        </div>
         <div 
           className={`${styles.hamburger} ${menuOpen ? styles.active : ''}`}
           onClick={toggleMenu}
